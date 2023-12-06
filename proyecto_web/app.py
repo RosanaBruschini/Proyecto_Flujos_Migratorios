766c8f7f05522861e805ea09fe705d6ae8994c87
Rosana <robruschini@gmail.com>
2023-12-06 12:05:58 -0300

    streamlit

--- proyecto_web/app.py
+++ proyecto_web/app.py
@@ -143,9 +143,9 @@ elif menu_option == "Dashboard":
                 tendencias, analiza patrones y mantenete actualizado con una herramienta esencial para 
                 comprender y tomar decisiones informadas sobre el fenómeno migratorio en la región.""")
     # Cargar y mostrar el contenido del dashboard de Power BI
-    #st.markdown('<iframe title="Report Section" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiY2IzNjg4NjMtNzVmYS00MDc1LTlkMjktY2Q0MmJhYTEyOTY4IiwidCI6IjFmODEwNTkyLTJiMTAtNGQyZi05ZDFkLWNhMzFiMjY5MTVkZSIsImMiOjR9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>')
-    st.markdown(
-    '<iframe title="Dashboard_Final_Flujos_Migratorios (1)" width="800" height="500" src="https://app.powerbi.com/reportEmbed?reportId=41c2cf4e-ec60-4764-96bb-74f248fff941&autoAuth=true&ctid=77f8c303-ef48-412f-bbb8-089de7b82c8a" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
+    st.markdown('<iframe title="Report Section" width="800" height="500" src="https://app.powerbi.com/view?r=eyJrIjoiY2IzNjg4NjMtNzVmYS00MDc1LTlkMjktY2Q0MmJhYTEyOTY4IiwidCI6IjFmODEwNTkyLTJiMTAtNGQyZi05ZDFkLWNhMzFiMjY5MTVkZSIsImMiOjR9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>')
+   # mio st.markdown(
+   # '<iframe title="Dashboard_Final_Flujos_Migratorios (1)" width="800" height="500" src="https://app.powerbi.com/reportEmbed?reportId=41c2cf4e-ec60-4764-96bb-74f248fff941&autoAuth=true&ctid=77f8c303-ef48-412f-bbb8-089de7b82c8a" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
     # src="https://app.powerbi.com/view?r=eyJrIjoiY2IzNjg4NjMtNzVmYS00MDc1LTlkMjktY2Q0MmJhYTEyOTY4IiwidCI6IjFmODEwNTkyLTJiMTAtNGQyZi05ZDFkLWNhMzFiMjY5MTVkZSIsImMiOjR9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>',
     #unsafe_allow_html=True
      
